a07646f237a8375b6e47daf072258208323c3d39
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-31 05:48:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+class Cat {
+  constructor(name, sex){
+    this.name=name;
+    this.sex=sex;
+  }
+  speak(){console.log("${this.name} says meow!")
+}
+ 
+class Dog {
+  constructor(name, sex){
+    this.name=name;
+    this.sex=sex;
+  }
+}
+class Bird {
+  constructor(name, sex){
+    this.name=name;
+    this.sex=sex;
+  }
+}
